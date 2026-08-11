02946de697ab4dae916dba8754022a49640595fd
UrosVel97 <velickovic97@gmail.com>
2026-08-11 13:20:47 +0200

    Refactor CreateProduct records and handler formatting
    Removed XML documentation comments from CreateProductCommand and CreateProductResult records. Reformatted these records to be more concise and inline. Combined ICommandHandler implementation in CreateProductCommandHandler into a single line for improved readability.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -3,14 +3,7 @@ using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct;
 
-/// <summary>
-/// CreateProductCommand represents the data that we need to create a new product
-/// </summary>
-/// <param name="Name">The name of the product</param>
-/// <param name="Category">The categories the product belongs to</param>
-/// <param name="Description">The description of the product</param>
-/// <param name="ImageFile">The image file of the product</param>
-/// <param name="Price">The price of the product</param>
+
 public record CreateProductCommand(
     string Name,
     List<string> Category,
@@ -18,14 +11,10 @@ public record CreateProductCommand(
     string ImageFile,
     decimal Price) : ICommand<CreateProductResult>;
 
-/// <summary>
-/// CreateProductResult represents a response/return object which in this case only returns a product id.
-/// </summary>
-/// <param name="Id"></param>
+
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : 
-    ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(
         CreateProductCommand command, 
